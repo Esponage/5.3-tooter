476b10604cded6ea571ddb121d1b43ed660904a5
Michael Esposito <michaelde1984@gmail.com>
2015-09-24 12:45:34 -0400

    initial commit

---
+++ app/scripts/models.js
@@ -0,0 +1,13 @@
+function Post() {
+}
+
+Post.fetch = function(){
+  return new $.ajax("http://tiny-lasagna-server.herokuapp.com/collections/posts").then(function(posts){
+    $(document).trigger('posts:fetched', [posts]);
+    return posts;
+  });
+};
+
+
+
+window.Post = Post;
